75aa89dc5734c78b8dee41554e6e4df4f2fd8545
bohdan <bohdanstep23@gmail.com>
2023-03-19 19:11:22 +0300

    deny

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,1 +1,10 @@
+number = int(input("ввести число: "))
 
+num_1= number % 10
+num_2 = (number // 10) % 10
+num_3 = (number // 100) % 10
+num_4 = (number // 1000) % 10
+
+
+reversed_number = num_1 * 1000 + num_2 * 100 + num_3 * 10 + num_4
+print ("результат:", reversed_number)
